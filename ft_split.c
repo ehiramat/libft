6e80ae5aac66bba3894a0bc6e0435a8891d83ae9
yuki <yukikiki521@icloud.com>
2020-11-02 23:28:04 +0900

    norm直したよ

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: ehiramat <ehiramat@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 17:31:23 by ehiramat          #+#    #+#             */
-/*   Updated: 2020/10/27 14:03:54 by ehiramat         ###   ########.fr       */
+/*   Updated: 2020/11/02 23:26:33 by yikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	mem_free(char **res, int k)
+int		mem_free(char **res, int k)
 {
 	while (k >= 0)
 	{
@@ -23,7 +23,7 @@ int	mem_free(char **res, int k)
 	return (0);
 }
 
-int	ft_subsplit(char **res, char *via, int len, int c)
+int		ft_subsplit(char **res, char *via, int len, int c)
 {
 	int	i;
 	int	k;
@@ -33,17 +33,17 @@ int	ft_subsplit(char **res, char *via, int len, int c)
 	{
 		i = 0;
 		while (via[i] != c && via[i] != '\0')
-	{
-	i++;
-	}
-	if (i > 0)
-	{
-		if (!(res[k] = ft_substr(via, 0, i)))
-		return (mem_free(res, k));
-		k++;
-	}
-	i++;
-	via = via + i;
+		{
+			i++;
+		}
+		if (i > 0)
+		{
+			if (!(res[k] = ft_substr(via, 0, i)))
+				return (mem_free(res, k));
+			k++;
+		}
+		i++;
+		via = via + i;
 	}
 	res[k] = 0;
 	return (1);
@@ -53,17 +53,17 @@ char	**ft_split(char const *s, char c)
 {
 	char	*via;
 	char	**res;
-	int	len;
-	int	i;
+	int		len;
+	int		i;
 
 	if (!(via = ft_strtrim(s, &c)))
-	return (NULL);
+		return (NULL);
 	len = 0;
 	i = 0;
 	while (via[i] != '\0')
 	{
 		if (via[i] == c && via[i + 1] != c)
-		len++;
+			len++;
 		i++;
 	}
 	len++;
